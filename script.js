446460cda16694d816af64968585362f35b9d97d
Rodrigo Cezar Leão <rodrigoczleo@yahoo.com.br>
2023-11-18 15:53:09 -0300

    start and stop clock with user configuration

--- script.js
+++ script.js
@@ -1,12 +1,76 @@
-let timeTeamA = 180;
-let timeTeamB = 180;
+var clockInterval;
 
-let intervalTeamA;
-let intervalTeamB;
+function getClockTime(id) {
+  let element = document.getElementById(id);
 
-const resumeTeamA = () => {
-    intervalTeamA = setInterval(() => {
-        timeTeamA--;
-        console.log(timeTeamA);
-    }, 1000);    
+  if (element.localName === "div") return element.innerText;
+  else if (element.localName === "input") return element.value;
+}
+
+function setClockTime(id, time) {
+  document.getElementById(id).innerText = time;
+}
+
+function convertTimeToSeconds(time) {
+  return Number(time.split(":")[0]) * 60 + Number(time.split(":")[1]);
+}
+
+function convertSecondsToTime(seconds) {
+  return (
+    (Math.trunc(seconds / 60) + "").padStart(2, "0") +
+    ":" +
+    ((seconds % 60) + "").padStart(2, "0")
+  );
+}
+
+function pauseClocks() {
+  clearInterval(clockInterval);
+}
+
+function startClocks() {
+  clockInterval = setInterval(() => updateClock(), 1000);
+}
+
+function updateClock() {
+  // Capturar informações dos relógios
+  let timeA = getClockTime("teamA");
+  let timeB = getClockTime("teamB");
+
+  // Converter para segundos numéricos
+  let timeA_seconds = convertTimeToSeconds(timeA);
+  let timeB_seconds = convertTimeToSeconds(timeB);
+
+  // Subtrair 1 segundo
+  timeA_seconds--;
+  timeB_seconds--;
+
+  // Converter para tempo formatado
+  let timeA_formatted = convertSecondsToTime(timeA_seconds);
+  let timeB_formatted = convertSecondsToTime(timeB_seconds);
+
+  // Atualizar no relógio
+  setClockTime("teamA", timeA_formatted);
+  setClockTime("teamB", timeB_formatted);
+}
+
+function resetClockConfigs() {
+  // Capturar informações dos Inputs
+  let timeA = getClockTime("time-config-teamA");
+  let timeB = getClockTime("time-config-teamB");
+
+  // Validar dados
+  if (timeA === "" || timeB === "")
+    window.alert("Os valores definidos para os relógios não são válidos!");
+
+  // Tratar dados recebidos
+  let timeA_seconds = convertTimeToSeconds(timeA);
+  let timeB_seconds = convertTimeToSeconds(timeB);
+
+  // Converter os dados para apresentar (formatar)
+  let timeA_formatted = convertSecondsToTime(timeA_seconds);
+  let timeB_formatted = convertSecondsToTime(timeB_seconds);
+
+  // Atualizar na tela
+  setClockTime("teamA", timeA_formatted);
+  setClockTime("teamB", timeB_formatted);
 }
